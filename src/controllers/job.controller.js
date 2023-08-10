ab30f65ac207e2e3b986497439c9f9575cbfd54a
Taran Mittal <taranmittal.1717@gmail.com>
2023-08-10 21:05:39 +0530

    added comments

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -9,27 +9,73 @@ import {
   updateJob,
 } from "../models/job.model.js";
 
+/**
+ * Controller class for handling job-related operations.
+*/
+
 export default class JobControl {
+
+  /**
+   * Renders the landing page.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+
   renderLandingPage = (req, res) => {
     res.render("landing-page", { user: req.session.user });
   };
+
+  /**
+   * Retrieves all job listings and renders the jobs page.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+
   getJobs = (req, res) => {
     let jobs = getAllJobs();
     res.render("list-all-jobs", { jobs, user: req.session.user });
     // res.render("product", { products, userEmail: req.session.userEmail });
   };
+
+  /**
+   * Creates a new job listing and redirects to the jobs page.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+
   newjob = (req, res) => {
     createNewJob(req.body);
     res.redirect("/jobs");
   };
+
+  /**
+   * Renders the new job form.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+
   renderJobForm = (req, res) => {
     res.render("new-job", { user: req.session.user });
   };
+
+  /**
+   * Finds a job listing by ID and renders the job details page.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+
   findJobById = (req, res) => {
     const id = req.params.id;
     const jobaData = findJobById(id);
     res.render("job-details", { data: jobaData, user: req.session.user });
   };
+
+  /**
+   * Adds a new applicant for a job and sends a confirmation email.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+
   newApplicant = async (req, res) => {
     const id = req.params.id;
     const { name, email, contact } = req.body;
@@ -38,6 +84,13 @@ export default class JobControl {
     await sendConfirmationMail(email);
     res.redirect("/jobs");
   };
+
+  /**
+   * Retrieves all applicants for a job and renders the applicants page.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+
   allApplicants = (req, res) => {
     const id = req.params.id;
     const resp = sendAllApplicants(id);
@@ -46,19 +99,45 @@ export default class JobControl {
       user: req.session.user,
     });
   };
+
+  /**
+   * Renders the update job form.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+
   renderUpdateform = (req, res) => {
     const id = req.params.id;
     const resp = findJobById(id);
     res.render("update-job", { job: resp });
   };
+
+  /**
+   * Updates a job listing by ID and redirects to the job details page.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+  
   updateJobById = (req, res) => {
     const id = req.params.id;
     updateJob(id, req.body);
     res.redirect(`/job/${id}`);
   };
+
+  /**
+   * Deletes a job listing by ID and redirects to the jobs page.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+   */
+
   deleteJob = (req, res) => {
     const id = req.params.id;
     deleteJob(id);
     res.redirect("/jobs");
   };
 }
+
+
+
+
+
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,39 @@
 import UserModel from "../models/user.model.js";
+
+
+// Controller class for handling user-related operations.
+
 export default class UserController {
+
+/**
+  Renders the login page.
+  * @param {Request} req - Express Request object.
+  * @param {Response} res - Express Response object.
+  * @param {NextFunction} next - Express Next function.
+*/
+
   getLogin = (req, res, next) => {
     res.render("user-login", { errors: null });
   };
+
+
+/**
+  * Adds a new user to the database and redirects to the login page.
+  * @param {Request} req - Express Request object.
+  * @param {Response} res - Express Response object.
+*/
+
+
   addUser = (req, res) => {
     UserModel.addUser(req.body);
     res.redirect("/login");
   };
+
+/**
+   * Authenticates the user's login credentials and redirects to appropriate pages.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+ */
   loginUser = (req, res) => {
     const { email, password } = req.body;
     const userToAuthenticate = UserModel.confirmLogin(req.body);
@@ -25,6 +52,12 @@ export default class UserController {
       res.render("404", { msg: "invalid credentials" });
     }
   };
+
+/**
+   * Logs out the current user and redirects to the login page.
+   * @param {Request} req - Express Request object.
+   * @param {Response} res - Express Response object.
+*/
   logoutUser = (req, res) => {
     req.session.userEmail = null;
     res.clearCookie("lastVisit");
