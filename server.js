34d2ef367ba463f5adacbba09657fb426844459e
Taran Mittal <taranmittal.1717@gmail.com>
2023-08-14 12:26:02 +0530

    snapshots

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Import the 'app' instance from the 'index.js' file
 import app from "./index.js";
 
+
 // Start the server on port 3200 and log a message when it's listening
 app.listen(3200, () => {
-  console.log("Server is listening on port 3200");
+  console.log(`Server is listening on localhost:${3200}`);
 });
